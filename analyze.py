ca77ca6d53cdc7a09b8dcf5c9e4351519dfb327f
Charles Lu <charleslu@live.com>
2016-05-04 15:36:28 -0700

    Switch to basemap implementation

--- analyze.py
+++ analyze.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import numpy as np
 
+import plot_map
+
 df = pd.read_csv('data/USGS_Produced_Waters_v2.2n.csv', index_col='IDUSGS')
-cols = ['DEPTHUPPER', 'TDS']
+cols = ['LATITUDE', 'LONGITUDE', 'DEPTHUPPER', 'TDS']
+MAP_OUTPUT = 'maps/wells_depth-{}_tds-{}.png'
 
 print(df[cols].describe())
 
-print('==== NUMBER OF DATA POINTS ====')
 for depth in (1000, 2000, float('inf')):
     for tds in (1000, 3000, 10000, float('inf')):
-        print('DEPTHUPPER < {} ft, TDS < {} mg/L: {}'.format(depth, tds, len(df[(df.DEPTHUPPER < depth) & (df.TDS < tds)])))
+        filtered = df[(df.DEPTHUPPER < depth) & (df.TDS < tds)]
+        print('DEPTHUPPER < {} ft, TDS < {} mg/L: {}'.format(depth, tds, len(filtered)))
+        plot_map.make_map(filtered[['LATITUDE', 'LONGITUDE']].dropna(), 
+                          MAP_OUTPUT.format(depth, tds), 
+                          'Wells with DEPTHUPPER < {} ft, TDS < {} mg/L'.format(depth, tds))
---
+++ plot_map.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+def make_map(df, filename, title):
+    plt.figure()
+    us_map = Basemap(llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49, 
+                  projection='lcc', lat_1=33, lat_2=45, lon_0=-95,
+                  resolution='i')
+    us_map.bluemarble(alpha=0.8)
+    us_map.drawcoastlines(color='#555566')
+    us_map.drawcountries()
+    us_map.drawstates()
+    x, y = us_map(df['LONGITUDE'].values, df['LATITUDE'].values)
+    plt.scatter(x, y, s=0.5, color='r', alpha=0.4, zorder=10)
+    plt.title(title)
+    plt.savefig(filename)
