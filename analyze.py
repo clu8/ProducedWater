a168af1449aa4321caa48f092d03729452ccc1b4
Charles Lu <charleslu@live.com>
2016-05-11 18:28:19 -0700

    Make CA maps

--- analyze.py
+++ analyze.py
@@ -5,7 +5,8 @@ import seaborn
 
 import maps
 
-MAP_OUTPUT = 'maps/wells_depth-{}_tds-{}.png'
+US_MAP_OUTPUT = 'maps/us_wells_depth-{}_tds-{}.png'
+CA_MAP_OUTPUT = 'maps/ca_wells_depth-{}_tds-{}.png'
 DESCRIBE_COLS = ['DEPTHUPPER', 'TDS']
 
 def describe():
@@ -17,9 +18,18 @@ def make_us_maps():
         for tds in (1000, 3000, 10000, float('inf')):
             filtered = df[(df.DEPTHUPPER < depth) & (df.TDS < tds)].dropna(subset=['LATITUDE', 'LONGITUDE'])
             print('\nDEPTHUPPER < {} ft, TDS < {} mg/L: {}'.format(depth, tds, len(filtered)))
-            print('Unique WELLNAME: {}'.format(filtered['WELLNAME'].nunique()))
-            #maps.make_map(filtered, MAP_OUTPUT.format(depth, tds),
-            #              'Wells with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(depth, tds, len(filtered)))
+            print('Unique WELLNAME: {}'.format(filtered.WELLNAME.nunique()))
+            maps.make_us_map(filtered, US_MAP_OUTPUT.format(depth, tds),
+                             'Wells with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(depth, tds, len(filtered)))
+
+def make_ca_maps():
+    for depth in (1000, 2000, float('inf')):
+        for tds in (1000, 3000, 10000, float('inf')):
+            filtered = df[(df.DEPTHUPPER < depth) & (df.TDS < tds) & (df.STATE == 'California')].dropna(subset=['LATITUDE', 'LONGITUDE'])
+            print('\nDEPTHUPPER < {} ft, TDS < {} mg/L: {}'.format(depth, tds, len(filtered)))
+            print('Unique WELLNAME: {}'.format(filtered.WELLNAME.nunique()))
+            maps.make_ca_map(filtered, CA_MAP_OUTPUT.format(depth, tds),
+                             'Wells in CA with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(depth, tds, len(filtered)))
 
 def make_dates_histogram():
     plt.figure()
@@ -33,3 +43,5 @@ df = pd.read_csv('data/USGS_Produced_Waters_v2.2n.csv',
                  index_col='IDUSGS',
                  parse_dates=['DATECOMP', 'DATESAMPLE', 'DATEANALYS'])
 print('Data loaded into pandas')
+make_us_maps()
+make_ca_maps()
