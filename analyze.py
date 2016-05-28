adc0d90de00cb176b11e70ce8f00fa06dcbc40c9
Charles Lu <charleslu@live.com>
2016-05-27 23:34:33 -0700

    Generalize code

--- analyze.py
+++ analyze.py
@@ -37,19 +37,15 @@ def make_us_maps():
                      'Wells with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(depth, tds, len(filtered)),
                      US_BASEMAP_KWARGS, US_SCATTER_KWARGS)
 
-CA_MAP_OUTPUT = 'maps/ca_wells_depth-{}_tds-{}.png'
-CA_BASEMAP_KWARGS = {'resolution': 'h', 'projection': 'lcc', 'width': 1284000, 'height': 1164000,
-                     'lat_1': 30, 'lat_2': 60, 'lat_0': 37, 'lon_0': -120.5, 'rsphere': 6370000}
-CA_SCATTER_KWARGS = {'alpha': 0.4, 's': 5}
-def make_ca_maps():
+def make_state_maps(state, output, basemap_kwargs, scatter_kwargs):
     for depth in (1000, 2000, float('inf')):
         for tds in (1000, 3000, 10000, float('inf')):
-            filtered = df[(df.DEPTHUPPER < depth) & (df.TDS < tds) & (df.STATE == 'California')].dropna(subset=['LATITUDE', 'LONGITUDE'])
+            filtered = df[(df.DEPTHUPPER < depth) & (df.TDS < tds) & (df.STATE == state)].dropna(subset=['LATITUDE', 'LONGITUDE'])
             print('\nDEPTHUPPER < {} ft, TDS < {} mg/L: {}'.format(depth, tds, len(filtered)))
             print('Unique WELLNAME: {}'.format(filtered.WELLNAME.nunique()))
-            make_map(filtered, CA_MAP_OUTPUT.format(depth, tds),
-                     'Wells in CA with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(depth, tds, len(filtered)),
-                     CA_BASEMAP_KWARGS, CA_SCATTER_KWARGS)
+            make_map(filtered, output.format(depth, tds),
+                     'Wells in {} with DEPTHUPPER < {} ft, TDS < {} mg/L: n = {}'.format(state, depth, tds, len(filtered)),
+                     basemap_kwargs, scatter_kwargs)
 
 def make_us_dates_histogram():
     plt.figure()
@@ -60,16 +56,22 @@ def make_us_dates_histogram():
     plt.savefig('plots/us_sample_years.png')
     plt.close()
 
-def make_ca_dates_histogram():
+def make_state_dates_histogram(state, output):
     plt.figure()
-    df[df.STATE == 'California'].dropna(subset=['LATITUDE', 'LONGITUDE', 'DEPTHUPPER', 'TDS', 'DATESAMPLE']).DATESAMPLE.dt.year.astype(int).hist(bins=130)
-    plt.title('Sample years, CA only')
+    df[df.STATE == state].dropna(subset=['LATITUDE', 'LONGITUDE', 'DEPTHUPPER', 'TDS', 'DATESAMPLE']).DATESAMPLE.dt.year.astype(int).hist(bins=130)
+    plt.title('Sample years, {} only'.format(state))
     plt.ylabel('Samples')
     plt.xlabel('Year')
-    plt.savefig('plots/ca_sample_years.png')
+    plt.savefig(output)
     plt.close()
 
 df = pd.read_csv('data/USGS_Produced_Waters_v2.2n.csv', 
                  index_col='IDUSGS',
                  parse_dates=['DATECOMP', 'DATESAMPLE', 'DATEANALYS'])
 print('Data loaded into pandas!')
+
+CA_MAP_OUTPUT = 'maps/ca_wells_depth-{}_tds-{}.png'
+CA_BASEMAP_KWARGS = {'resolution': 'h', 'projection': 'lcc', 'width': 1284000, 'height': 1164000,
+                     'lat_1': 30, 'lat_2': 60, 'lat_0': 37, 'lon_0': -120.5, 'rsphere': 6370000}
+CA_SCATTER_KWARGS = {'alpha': 0.4, 's': 5}
+CA_HIST_OUTPUT = 'plots/ca_sample_years.png'
